501390cbd06d472f7313e76fb8b9aa4aa2045061
Kit Plummer <kitplummer@gmail.com>
2022-12-25 11:29:38 -0500

    add: config tests

--- src/config.rs
+++ src/config.rs
@@ -9,10 +9,23 @@ pub struct Config {
 }
 
 pub fn read_config(path: &Path) -> Config {
-    let mut file = File::open(path).unwrap();
+    let mut file = match File::open(path) {
+        Ok(file) => file,
+        Err(_) => {
+            println!("LEI error: could not read config file.");
+            std::process::exit(1);
+        }
+    };
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
-    let config: Config = toml::from_str(&contents).unwrap();
+    let config: Config = match toml::from_str(&contents) {
+        Ok(contents) => contents,
+        Err(error) => {
+            println!("LEI error: could not read config contents - {}.", error);
+            std::process::exit(1);
+
+        }
+    };
     config
 }
 
--- tests/cli.rs
+++ tests/cli.rs
@@ -3,16 +3,40 @@ use assert_cmd::prelude::*; // Add methods on commands
 use std::process::Command; // Run programs
 
 #[test]
-fn test_default_configuration_write() -> Result<(), Box<dyn std::error::Error>> {
+fn test_default_configuration_file_error() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("lei")?;
+    cmd.arg("-c");
+    cmd.arg("lei.toml");
     cmd.arg("analyze");
     cmd.arg("https://github.com/kitplummer/gbtestee");
     cmd.assert()
         //.failure()
-        .stdout(predicates::str::contains("Analyzing https://github.com/kitplummer/gbtestee"));
+        .stdout(predicates::str::contains("could not read config file"));
     Ok(())
 }
 
+#[test]
+fn test_default_configuration_content_error() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("lei")?;
+    cmd.arg("-c");
+    cmd.arg("tests/lei.toml");
+    cmd.arg("analyze");
+    cmd.arg("https://github.com/kitplummer/gbtestee");
+    cmd.assert()
+        //.failure()
+        .stdout(predicates::str::contains("could not read config contents"));
+    Ok(())
+}
+
+#[test]
+fn test_basic_analysis_post() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("lei")?;
+    cmd.arg("analyze");
+    cmd.arg("https://github.com/kitplummer/gbtestee");
+    cmd.assert()
+        .stdout(predicates::str::contains("complete"));
+    Ok(())
+}
 #[test]
 fn test_help() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("lei")?;
